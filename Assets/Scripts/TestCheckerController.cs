613270fa85e298084980c1dafd28b12584f6da0c
Aiden C. Desjarlais <aiden.desjarlais2013@gmail.com>
2022-11-09 23:30:43 -0600

    fixed the checker spawning and their offsets.
    re installed Mirror Networking due to version errors, and updated the checker controller to Keirs new version of it.
    
    TODO: Rename TestCheckerController to CheckerController

--- Assets/Scripts/TestCheckerController.cs
+++ Assets/Scripts/TestCheckerController.cs
@@ -1,74 +1,44 @@
-/* Copyright {C} 2022 Aiden Desjarlais
- * Copyright {C} 2022 Keir Yurkiw
-*/
+/* Copyright (C) 2022 Aiden Desjarlais
+ * Copyright (C) 2022 Keir Yurkiw */
 
-
-
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System;
-using UnityEditor;
 
 namespace CheckerController
 {
-
     public class TestCheckerController : MonoBehaviour
     {
         public GameObject board;
         public GameObject checker_prefab;
 
-        public float next_x = 0;
-        private Vector3[] checker_pos = new Vector3[8 * 8];
-
+        private Vector3[] checker_pos = new Vector3[8*8];
 
         private bool
-        is_even(int i)
+        is_even(uint i)
         {
-            return (i%2) == 0;
+            return i % 2 == 0;
         }
 
-        private void Awake()
+        private void
+        Awake()
         {
             const float num_cells = 8.0f;
             float cell_size = board.GetComponent<Renderer>().bounds.size.x / num_cells;
 
-            float board_offset = board.GetComponent<Renderer>().bounds.size.x / 2.0f;
-            GameObject temp_checker = Instantiate(checker_prefab);
-            float checker_offset = temp_checker.GetComponent<Renderer>().bounds.size.x / 2.0f;
-            Destroy(temp_checker);
+            Vector3 bottom_left_cell_pos = new Vector3(
+                    /* get position of the bottom-left corner of board and add cell
+                     * offset to move to the center of the bottom-left cell */
+                    -board.GetComponent<Renderer>().bounds.size.x/2.0f + cell_size/2.0f,
+                    0.0f,
+                    -board.GetComponent<Renderer>().bounds.size.z/2.0f + cell_size/2.0f);
 
-            for (var i = 0; i < num_cells; i++)
-            {
-                for (var j = 0; j < num_cells; j++)
-                {
+            for (uint i = 0; i < num_cells; i++) {
+                for (uint j = 0; j < num_cells; j++) {
+                    checker_pos[i+j].x = bottom_left_cell_pos.x + (cell_size * i);
+                    checker_pos[i+j].y = bottom_left_cell_pos.y;
+                    checker_pos[i+j].z = bottom_left_cell_pos.z + (cell_size * j);
 
-                    // if ((j == 6 || j == 3) || is_even(j))
-                    //     continue;
-
-                    switch (j)
-                    {
-                        case 0:
-                        case 2:
-                        case 4:
-                        case 5:
-                        case 6:
-                        case 8:
-                            continue;
-                    }
-
-                    checker_pos[i].x = cell_size * i - board_offset + checker_offset + 0.1f;
-                    checker_pos[i].z = cell_size * j - board_offset + checker_offset + 0.1f;
-                    checker_pos[i].y = 0.233f;
-
-                    if (!is_even(i))
-                    {
-                        checker_pos[i].z -= cell_size;
-                    }
-                        
-
-                    //print(checker_pos[i].x + " " + checker_pos[i].y + " " + checker_pos[i].z);
-                    Instantiate(checker_prefab).transform.position = checker_pos[i];
+                    if (!is_even(i+j) && (j != 3 && j != 4))
+                        Instantiate(checker_prefab).transform.position = checker_pos[i+j];
                 }
             }
         }
